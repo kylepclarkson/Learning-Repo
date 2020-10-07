8c233e65768e48959f76db538a054459b6a40bd2
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-10-06 22:08:01 -0700

    Update SuffixTree.py

--- suffix_trees/GST/trees/SuffixTree.py
+++ suffix_trees/GST/trees/SuffixTree.py
@@ -10,6 +10,9 @@ class SuffixTree(Tree):
             self._parent = parent
             self._children = {} if children is None else children
 
+        def __repr__(self):
+            return  str(self._element)
+
     # === An abstract representation of a element in the tree.
     class Position(Tree.Position):
 
@@ -34,6 +37,10 @@ class SuffixTree(Tree):
 
         if pos._container is not self:
             raise ValueError('pos does not belong to this container')
+
+        # convention: when removing a node, set its parent to itself.
+        if pos._node._parent is pos._node:
+            raise ValueError('pos is no longer valid')
 
         return pos._node
 
@@ -105,16 +112,78 @@ class SuffixTree(Tree):
         node._element = e
         return old
 
+    def _remove(self, pos):
+        """ Delete the node at Position pos.
+        If pos has a single child, make the child's parent pos.
+        If pos has more than one child, throw error.
+        Return element that was stored at pos.
+        """
+        node = self._validate(pos)
+
+        if self.num_children(pos) > 1:
+            raise ValueError('pos have more than one child')
+
+        if len(node._children) == 0:
+            node._parent = node         # deprecate node.
+        else:
+            # single child. Swap
+            child = node._children[0]
+            child._parent = node._parent
+            self._size -= 1
+            node._parent = node         # deprecate node.
+        return node._element
+
+    def _insert_between(self, pos_parent, pos_child, e):
+        """ Insert new Position with element e in between pos_parent and pos_child.
+        pos_child will be a child of the new Position, and the new Position will
+        be a child of pos_parent.
+        Return Position of new node.
+        """
+
+        parent_node = self._validate(pos_parent)
+        child_node = self._validate(pos_child)
+        # Check that parent-child relationship holds.
+        if not child_node in parent_node._children:
+            raise ValueError('pos_parent is not a parent of pos_child')
+        self._size += 1
+        new_node = self._Node(e, parent_node)
+        child_node._parent = new_node
+        return self._make_position(new_node)
 
 # Testing
-t = SuffixTree('root')
-root = t.root()
-b = t._add(root, 'b')
-c = t._add(root, 'c')
-d = t._add(root, 'd')
-e = t._add(b, 'e')
-f = t._add(e, 'f')
-g = t._add(e, 'g')
+def test1():
+    t = SuffixTree()
+    root = t.root()
+    b = t._add(root, 'b')
+    c = t._add(root, 'c')
+    d = t._add(root, 'd')
+    e = t._add(b, 'e')
+    f = t._add(e, 'f')
+    g = t._add(e, 'g')
 
-h = t._replace(e, 'h')
-[print(c.element()) for c in t.children(b)]
+    h = t._replace(e, 'h')
+    [print(c.element()) for c in t.children(b)]
+
+def test2():
+    t = SuffixTree()
+    root = t.root()
+
+    # insert axabx
+    x1 = t._add(root, 'axabx')
+
+    # insert xabx
+    x2 = t._add(root, 'xabx')
+
+    # TODO implement in _insert_between
+    x3 = t._add(x1, 'xabx')
+    x4 = t._add(x1, 'bx')
+    t._replace(x1, 'a')
+
+    print('root')
+    # [print(c.element()) for c in t.children(root)]
+    print(t._validate(x1)._children)
+    print('x1')
+    [print(c.element()) for c in t.children(x1)]
+
+
+test2()
