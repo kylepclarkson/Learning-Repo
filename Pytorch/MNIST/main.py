1c8216ac3c69d218cc6697b1d84661450d5d7bf3
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-10-23 12:20:17 -0700

    PyTorch created project to train models on MNIST dataset.

---
+++ Pytorch/MNIST/main.py
@@ -0,0 +1,13 @@
+import torch
+
+"""
+TODO 
+
+    - Read in data, 
+    - Define network 
+    - Training loop
+    - Function for plotting learning curve
+    - Func for running test set, counting validations. 
+
+"""
+
---
+++ Pytorch/MNIST/models.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class AppaNet(nn.Module):
+
+    """
+    TODO:
+        - Define network (init)
+            - Initialize parameters
+        - Define forward function
+        - Define loading/saving functions
+    """
+    def __init__(self):
+        super(AppaNet, self).__init__()
