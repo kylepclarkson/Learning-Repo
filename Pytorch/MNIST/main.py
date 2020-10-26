f8423334562fb9dd323b015dbcb4bb2c514bec79
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-10-26 16:41:19 -0700

    PyTorch MNIST
    Trained Appa net for classification of digits. Achieved 95% validation accuracy. Created  notebook to test model, display images and the model predictions.

--- Pytorch/MNIST/main.py
+++ Pytorch/MNIST/main.py
@@ -1,5 +1,11 @@
+import os
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
+import torchvision
 
+import models
 """
 TODO 
 
@@ -18,4 +24,99 @@ TODO
         
 
 """
+def training_loop(n_epochs,
+                  model,
+                  optimizer,
+                  loss_function,
+                  train_loader,
+                  val_loader):
+    print('Training:')
+    best_val_accuracy = 0
+    running_loss = []
+
+    for epoch in range(1, n_epochs+1):
+
+        training_loss = 0.0
+
+        # iterate over training batch
+        for imgs, labels_true in train_loader:
+            # move tensors to device
+            imgs = imgs.to(model.device)
+            labels_true = labels_true.to(model.device)
+
+            labels_pred = model(imgs)
+
+            loss = loss_function(labels_pred, labels_true)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            training_loss += loss.item()
+
+        running_loss.append(training_loss)
+        # print training and validation losses. save if model achieve better accuracy rate.
+        if epoch % 10 == 0 or epoch == 1 or epoch == n_epochs:
+            loss = training_loss / len(train_loader)
+            print(f'{datetime.datetime.now()} Epoch: {epoch}, Training loss: {loss:.3f}')
+
+            training_accuracy = validate_model(model, train_loader)
+            validation_accuracy = validate_model(model, val_loader)
+            print(f'Training accuracy: {training_accuracy:.3f}')
+            print(f'Validation accuracy: {validation_accuracy:.3f}')
+
+            if validation_accuracy > best_val_accuracy:
+                # save model
+                print('Saving model!')
+                model.save_checkpoint()
+                best_val_accuracy = validation_accuracy
+
+    return running_loss
+
+def validate_model(model, data_loader):
+    # Count number of correctly predicted data items, divided by number of items.
+    correct_pred, total_data = 0, 0
+    with torch.no_grad():
+        for imgs, labels in data_loader:
+            imgs = imgs.to(model.device)
+            labels = labels.to(model.device)
+
+            labels_pred = model(imgs)
+            _, predicted = torch.max(labels_pred, dim=1)
+            total_data += labels.shape[0]
+            correct_pred += int((predicted == labels).sum())
+    return correct_pred / total_data
+
+def plot_loss(training_epochs, losses, figure_file):
+    plt.plot(training_epochs, losses)
+    plt.title('Training loss vs Epoch')
+    plt.savefig(figure_file)
+
+if __name__ == '__main__':
+
+    # TODO apply transform to normalize data
+    transforms = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor()
+    ])
+    # Get mnist data
+    data_path = 'mnist_data/'
+    training_set = torchvision.datasets.MNIST(data_path, train=True, download=True, transform=transforms)
+    training_loader = torch.utils.data.DataLoader(training_set, batch_size=32, shuffle=True)
+
+    validation_set = torchvision.datasets.MNIST(data_path, train=False, download=True, transform=transforms)
+    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=32, shuffle=False)
+
+    model = models.AppaNet()
+    print('Model parameters: ', sum(p.numel() for p in model.parameters()))
+
+    opt = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_function = torch.nn.CrossEntropyLoss()
+    n_epochs = 50
+
+    running_loss = training_loop(n_epochs=n_epochs,
+                                model=model,
+                                optimizer=opt,
+                                loss_function=loss_function,
+                                train_loader=training_loader,
+                                val_loader=validation_loader)
+
+    plot_loss(np.arange(1, n_epochs+1), running_loss, model.name)
 
--- Pytorch/MNIST/models.py
+++ Pytorch/MNIST/models.py
@@ -5,39 +5,32 @@ import torch.nn.functional as F
 
 class AppaNet(nn.Module):
 
-    """
-    TODO:
-        - Define network (init)
-            - Initialize parameters
-        - Define forward function
-        - Define loading/saving functions
-    """
-    def __init__(self, name='appa_net', checkpoint_dir='tmp/mnist'):
+    def __init__(self, name='appa', checkpoint_dir='models/'):
         super(AppaNet, self).__init__()
 
         self.name = name
         self.checkpoint_dir = checkpoint_dir
-        self.checkpoint_file = os.path.join(self.checkpoint_dir, self.name+'_mnist')
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, self.name+'_mnist.pt')
 
         # === Construct network ===
-        # convolve: 1x28x28 --> 8x28x28
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
-        # pool: 8x28x28 --> 8x14x14
+        # convolve: 1x28x28 --> 4x28x28
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1)
+        # pool: 4x28x28 --> 8x14x14
         self.pool1 = nn.MaxPool2d(2, 2)
         # dropout
         self.dropout1 = nn.Dropout2d(p=0.3)
 
-        # convolve: 8x14x14 --> 16x14x14
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        # pool: 16x14x14 -->  16x7x7
+        # convolve: 4x14x14 --> 8x14x14
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, padding=1)
+        # pool: 8x14x14 -->  8x7x7
         self.pool2 = nn.MaxPool2d(2, 2)
         # dropout
         self.dropout2 = nn.Dropout(p=0.3)
 
-        # fully connected: 16x7x7 --> 32x1
-        self.dense1 = nn.Linear(16*7*7, 32)
+        # fully connected: 8x7x7 --> 20x1
+        self.dense1 = nn.Linear(8*7*7, 20)
         # fully connected: 32x1 --> 10x1
-        self.dense2 = nn.Linear(32, 10)
+        self.dense2 = nn.Linear(20, 10)
 
         # Set device
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -49,7 +42,7 @@ class AppaNet(nn.Module):
         x = self.dropout1(self.pool1(F.relu(self.conv1(x))))
         x = self.dropout2(self.pool2(F.relu(self.conv2(x))))
 
-        x = x.view(-1, 16*7*7)
+        x = x.view(-1, 8*7*7)
         x = F.relu(self.dense1(x))
         x = self.dense2(x)
 
