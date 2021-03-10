9b65b4b8b7dd51dd0e81c638691a922fd583e620
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-10 07:53:09 -0800

    Node js
    Part 4. Custom url matching and redirects.

--- node-crash-course/server.js
+++ node-crash-course/server.js
@@ -1,9 +1,46 @@
 // import module
 const http = require('http');
+const fs = require('fs')
+
 // create server. Function is run everytime a request is made to the server.
 const server = http.createServer((request, response) => {
-    console.log('Request made.')
-    console.log(request.method)
+    // response.setHeader('Content-Type', 'text/plain') // send plain text
+    response.setHeader('Content-Type', 'text/html')
+
+    let path = './views';
+    switch(request.url) {
+        case '/':
+            path += '/index.html'
+            response.statusCode = 200;
+            break;
+        case '/about':
+            path += '/about.html'
+            response.statusCode = 200;
+            break;
+        case '/about-me':
+            response.statusCode = 301
+            // Redirect to /about
+            response.setHeader('Location', '/about')
+            response.end()
+            break;
+        default:
+            path += '/404.html'
+            response.statusCode = 404;
+            break;
+    }
+
+    // send html file as response
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            response.end();
+        } else {
+            response.write(data);
+            response.end()
+        }
+    })
+
+
 });
 
 // port, host, function on start
