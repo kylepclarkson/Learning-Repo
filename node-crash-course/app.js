4c9b4c9c163572b051c4288b1d682c42f5bbd99a
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-11 10:34:05 -0800

    Node.js
    Part 10 request methods.

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan')
 const mongoose = require('mongoose');
 // const { result } = require('lodash');
-const Blog = require('./models/blog')
+const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 
 // instace of express app.
@@ -18,10 +19,14 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
 // register view engine. Searches for 'views' directory by default.
 app.set('view engine', 'ejs');
 
+// === middleware ===
 // serve static files
 app.use(express.static('public'));
+// accress form data
+app.use(express.urlencoded({extended: true}));
 // morgan - logging tool
 app.use(morgan('dev'));
+
 
 
 // === sandbox 
@@ -56,10 +61,6 @@ app.get('/single-blog', (req, res) => {
 })
 
 
-
-
-
-
 app.get('/', (req, res) => {
     // auto-infers content type (sets header) and status code.
     // res.send('<p>Home Page</p>')
@@ -77,6 +78,53 @@ app.get('/', (req, res) => {
     res.redirect('/blogs')
 });
 
+/**
+ * Create new blog using form data.
+ */
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save()
+        .then((result) => {
+            // redirect user to homepage
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+/**
+ * Get blog post by its id.
+ */
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', {blog: result, title: 'Blog Details'})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            // as front end is using AJAX, we cannot respond with a redirect.
+            // respond with json pointing to where to go next.
+            res.json({
+                redirect: '/blogs'
+            });
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+/**
+ * Display all blog posts.
+ */
 app.get('/blogs', (req, res) => {
     Blog.find().sort({createdAt: -1})
         .then((result) => {
