d1bfde0eed210ad3c9d87440b0774d2332739b02
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-15 17:50:29 -0700

    codevolution 1-8

---
+++ JavaScript/codevolution/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="kyle" />
+    </div>
+  );
+}
+
+export default App;
