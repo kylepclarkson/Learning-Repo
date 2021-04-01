1b576a0bb047a7d9db18fca5add975e44086ca0d
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-04-01 14:59:01 -0700

    React todo app
    Added form submission.

--- React/todo-rest/src/App.js
+++ React/todo-rest/src/App.js
@@ -2,19 +2,115 @@ import React from 'react'
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoList: [],
+      // the item that will be updated
+      activeItem: {
+        id: null, title: "", completed: false
+      },
+      // if editing a post 
+      editing: false,
+    }
+
+    // bind fetchTasks function to class.
+    this.fetchTasks = this.fetchTasks.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.createCookie = this.createCookie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchTasks();
+  }
+
+  createCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i=0; i<cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length+1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+
+  fetchTasks() {
+    // fetch tasks from api.
+    console.log('Fetching...');
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+      .then(res => res.json())
+      .then(data =>
+        this.setState({
+          'todoList': data,
+        })
+      )
+  }
+
+  handleChange(e) {
+    // update title of task entered in form.
+
+    var name = e.target.name;
+    var title = e.target.value;
+
+    this.setState({
+      activeItem: {
+        ...this.state.activeItem,
+        title: title
+      }
+    })
+  }
+
+  handleSubmit(e) {
+    // submit form data. 
+    e.preventDefault();
+    console.log('Item: ', this.state.activeItem)
+    // generate cookie token 
+    var csrfToken = this.createCookie('csrftoken');
+
+    // submit the active item to api
+    var url = 'http://127.0.0.1:8000/api/task-create/'
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'X-CSRFToken': csrfToken,
+      }, 
+      body: JSON.stringify(this.state.activeItem)
+    }).then((res) => {
+      this.fetchTasks();
+      this.setState({
+        // clear item
+        activeItem: {
+          id: null, title: "", completed: false
+        },
+      })
+    }).catch((err) => console.log("Error submiting data: ", err))
+
+  }
+
   render() {
+    var tasks = this.state.todoList
     return (
       <div className="container">
         <div id="task-container">
           <div id="form-wrapper">
-            <form action="" id="id">
+            <form onSubmit={this.handleSubmit} id="id">
               <div className="flex-wrapper">
-                <div style={{flex: 6}}>
-                  <input type="text" className="form-control" id="title" name="title" placeholder="Add task" />
+                <div style={{ flex: 6 }}>
+                  <input type="text" onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} name="title" placeholder="Add task" />
                 </div>
 
-                <div style={{flex: 1}}>
-                  <input type="submit" className="btn btn-warning" id="submit" name="Add"/>
+                <div style={{ flex: 1 }}>
+                  <input type="submit" className="btn btn-warning" id="submit" name="Add" />
                 </div>
 
               </div>
@@ -22,7 +118,26 @@ class App extends React.Component {
           </div>
 
           <div id="list-wrapper">
-
+            {/* display tasks */}
+            {tasks.map(function (task, index) {
+              return (
+                <div
+                  key={index}
+                  className="task-wrapper flex-wrapper">
+                  <div style={{flex: 7}}>
+                    <span>{task.title}</span>
+                  </div>
+                  
+                  <div style={{flex: 1}}>
+                    <button className="btn btn-sm btn-outline-info">Edit</button>
+                  </div>
+                  
+                  <div style={{flex: 1}}>
+                    <button className="btn btn-sm btn-outline-dark">Remove</button>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
