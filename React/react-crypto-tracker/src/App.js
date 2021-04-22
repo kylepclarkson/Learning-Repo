5af7a77172daf5fdd1d9d8b6536494e2fd193bf5
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-04-22 13:47:02 -0700

    Created crypto tracker

---
+++ React/react-crypto-tracker/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import './App.css';
+import Coin from './components/Coin'
+
+function App() {
+
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(response => {
+      setCoins(response.data)
+      console.log(response.data)
+    })
+  }, [])
+
+  const handleChange = e => {
+    // set search string
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => {
+    // filter coins matching to search string
+    return coin.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input 
+            type="text" 
+            placeholder="Search" 
+            className="coin-input" 
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin 
+            key={coin.id} 
+            name={coin.name} 
+            image={coin.image}
+            symbol={coin.symbol} 
+            price={coin.current_price} 
+            marketcap={coin.market_cap}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
